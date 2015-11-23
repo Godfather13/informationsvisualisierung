ad996eb6f71eedae67bdce9deaf41a1bd2854a91
brehmju <julian.brehm@gmx.net>
2015-11-23 17:45:55 +0100

    add bootstrap and some d3-charts

---
+++ assets/js/script.js
@@ -0,0 +1,379 @@
+$("#year").change(function () {
+	
+	//alert($(this).val());
+	//year = $(this).val();
+});
+
+/* CHART 1 */
+
+var margin_1 = {top: 20, right: 40, bottom: 30, left: 20},
+    width_1 = document.getElementById('chart_1').offsetWidth - 50 - margin_1.left - margin_1.right,
+    height_1 = 500 - margin_1.top - margin_1.bottom,
+    barWidth_1 = Math.floor(width_1 / 19) - 1;
+
+var x_1 = d3.scale.linear()
+    .range([barWidth_1 / 2, width_1 - barWidth_1 / 2]);
+
+var y_1 = d3.scale.linear()
+    .range([height_1, 0]);
+
+var yAxis_1 = d3.svg.axis()
+    .scale(y_1)
+    .orient("right")
+    .tickSize(-width_1)
+    .tickFormat(function(d) { return Math.round(d / 1e6) + "M"; });
+
+// An SVG element with a bottom-right origin.
+var svg_1 = d3.select("#chart_1").append("svg")
+    .attr("width", width_1 + margin_1.left + margin_1.right)
+    .attr("height", height_1 + margin_1.top + margin_1.bottom)
+  .append("g")
+    .attr("transform", "translate(" + margin_1.left + "," + margin_1.top + ")");
+
+// A sliding container to hold the bars by birthyear.
+var birthyears = svg_1.append("g")
+    .attr("class", "birthyears");
+
+d3.csv("data/population.csv", function(error, data) {
+
+  // Convert strings to numbers.
+  data.forEach(function(d) {
+    d.people = +d.people;
+    d.year = +d.year;
+    d.age = +d.age;
+  });
+
+  // Compute the extent of the data set in age and years.
+  var age1 = d3.max(data, function(d) { return d.age; }),
+      year0 = d3.min(data, function(d) { return d.year; }),
+      year1 = d3.max(data, function(d) { return d.year; }),
+      year = year1;
+
+  // Update the scale domains.
+  x_1.domain([year1 - age1, year1]);
+  y_1.domain([0, d3.max(data, function(d) { return d.people; })]);
+
+  // Produce a map from year and birthyear to [male, female].
+  data = d3.nest()
+      .key(function(d) { return d.year; })
+      .key(function(d) { return d.year - d.age; })
+      .rollup(function(v) { return v.map(function(d) { return d.people; }); })
+      .map(data);
+
+  // Add an axis to show the population values.
+  svg_1.append("g")
+      .attr("class", "y axis")
+      .attr("transform", "translate(" + width_1 + ",0)")
+      .call(yAxis_1)
+    .selectAll("g")
+    .filter(function(value) { return !value; })
+      .classed("zero", true);
+
+  // Add labeled rects for each birthyear (so that no enter or exit is required).
+  var birthyear = birthyears.selectAll(".birthyear")
+      .data(d3.range(year0 - age1, year1 + 1, 5))
+    .enter().append("g")
+      .attr("class", "birthyear")
+      .attr("transform", function(birthyear) { return "translate(" + x_1(birthyear) + ",0)"; });
+
+  birthyear.selectAll("rect")
+      .data(function(birthyear) { return data[year][birthyear] || [0, 0]; })
+    .enter().append("rect")
+      .attr("x", -barWidth_1 / 2)
+      .attr("width", barWidth_1)
+      .attr("y", y_1)
+      .attr("height", function(value) { return height_1 - y_1(value); });
+
+  // Add labels to show birthyear.
+  birthyear.append("text")
+      .attr("y", height_1 - 4)
+      .text(function(birthyear) { return birthyear; });
+
+  // Add labels to show age (separate; not animated).
+  svg_1.selectAll(".age")
+      .data(d3.range(0, age1 + 1, 5))
+    .enter().append("text")
+      .attr("class", "age")
+      .attr("x", function(age) { return x_1(year - age); })
+      .attr("y", height_1 + 4)
+      .attr("dy", ".71em")
+      .text(function(age) { return age; });
+
+  window.focus();
+  /*
+  d3.select(window).on("keydown", function() {
+    switch (d3.event.keyCode) {
+      case 37: year = Math.max(year0, year - 10); break;
+      case 39: year = Math.min(year1, year + 10); break;
+    }
+    update_char_1();
+  });
+  */
+  	// change year
+		d3.select("#year").on("change", function() {
+
+			year = $("#year").val();
+			update_char_1();
+		});
+
+  
+  function update_char_1() {
+    if (!(year in data)) return;
+
+    birthyears.transition()
+        .duration(750)
+        .attr("transform", "translate(" + (x_1(year1) - x_1(year)) + ",0)");
+
+    birthyear.selectAll("rect")
+        .data(function(birthyear) { return data[year][birthyear] || [0, 0]; })
+      .transition()
+        .duration(750)
+        .attr("y", y_1)
+        .attr("height", function(value) { return height_1 - y_1(value); });
+  }
+});
+
+/* CHART 2 */
+
+var width = document.getElementById('chart_2').offsetWidth - 50,
+    height = 300,
+    radius = Math.min(width, height) / 2;
+
+var color = d3.scale.ordinal()
+    .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
+
+var arc = d3.svg.arc()
+    .outerRadius(radius - 10)
+    .innerRadius(0);
+
+var pie = d3.layout.pie()
+    .sort(null)
+    .value(function(d) { return d.population; });
+
+var svg_2 = d3.select("#chart_2").append("svg")
+    .attr("width", width)
+    .attr("height", height)
+  .append("g")
+    .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+d3.csv("data/data2.csv", function(error, data) {
+
+  data.forEach(function(d) {
+    d.population = +d.population;
+  });
+
+  var g = svg_2.selectAll(".arc")
+      .data(pie(data))
+    .enter().append("g")
+      .attr("class", "arc");
+
+  g.append("path")
+      .attr("d", arc)
+      .style("fill", function(d) { return color(d.data.age); });
+
+  g.append("text")
+      .attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")"; })
+      .attr("dy", ".35em")
+      .style("text-anchor", "middle")
+      .text(function(d) { return d.data.age; });
+
+});
+
+/* CHART 3 */
+
+var width = document.getElementById('chart_3').offsetWidth - 50,
+    height = 300,
+    radius = Math.min(width, height) / 2;
+
+var color = d3.scale.ordinal()
+    .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
+
+var arc = d3.svg.arc()
+    .outerRadius(radius - 10)
+    .innerRadius(0);
+
+var pie = d3.layout.pie()
+    .sort(null)
+    .value(function(d) { return d.population; });
+
+var svg_3 = d3.select("#chart_3").append("svg")
+    .attr("width", width)
+    .attr("height", height)
+  .append("g")
+    .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+d3.csv("data/data2.csv", function(error, data) {
+
+  data.forEach(function(d) {
+    d.population = +d.population;
+  });
+
+  var g = svg_3.selectAll(".arc")
+      .data(pie(data))
+    .enter().append("g")
+      .attr("class", "arc");
+
+  g.append("path")
+      .attr("d", arc)
+      .style("fill", function(d) { return color(d.data.age); });
+
+  g.append("text")
+      .attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")"; })
+      .attr("dy", ".35em")
+      .style("text-anchor", "middle")
+      .text(function(d) { return d.data.age; });
+
+});
+
+
+/* CHART 4 */
+
+var margin = {top: 30, right: 20, bottom: 30, left: 40},
+    width = document.getElementById('chart_4').offsetWidth - 50 - margin.left - margin.right,
+    height = 300 - margin.top - margin.bottom;
+	
+var formatPercent = d3.format(".0%");
+
+var x = d3.scale.ordinal()
+    .rangeRoundBands([0, width], .1);
+
+var y = d3.scale.linear()
+    .range([height, 0]);
+
+var xAxis = d3.svg.axis()
+    .scale(x)
+    .orient("bottom");
+
+var yAxis = d3.svg.axis()
+    .scale(y)
+    .orient("left")
+    .tickFormat(formatPercent);
+
+var tip = d3.tip()
+  .attr('class', 'd3-tip')
+  .attr('class', 'd3-tip')
+  .offset([-10, 0])
+  .html(function(d) {
+    return "<strong>Frequency:</strong> <span style='color:red'>" + d.frequency + "</span>";
+  })
+
+var svg_4 = d3.select("#chart_4").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+svg_4.call(tip);
+
+d3.tsv("data/data.tsv", type, function(error, data) {
+  x.domain(data.map(function(d) { return d.letter; }));
+  y.domain([0, d3.max(data, function(d) { return d.frequency; })]);
+
+  svg_4.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(xAxis);
+
+  svg_4.append("g")
+      .attr("class", "y axis")
+      .call(yAxis)
+    .append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 6)
+      .attr("dy", ".71em")
+      .style("text-anchor", "end")
+      .text("Frequency");
+
+  svg_4.selectAll(".bar")
+      .data(data)
+    .enter().append("rect")
+      .attr("class", "bar")
+      .attr("x", function(d) { return x(d.letter); })
+      .attr("width", x.rangeBand())
+      .attr("y", function(d) { return y(d.frequency); })
+      .attr("height", function(d) { return height - y(d.frequency); })
+      .on('mouseover', tip.show)
+      .on('mouseout', tip.hide)
+
+});
+
+function type(d) {
+  d.frequency = +d.frequency;
+  return d;
+}
+
+/* CHART 5 */
+
+var margin = {top: 30, right: 20, bottom: 30, left: 40},
+    width = document.getElementById('chart_5').offsetWidth - 50 - margin.left - margin.right,
+    height = 300 - margin.top - margin.bottom;
+	
+var formatPercent = d3.format(".0%");
+
+var x = d3.scale.ordinal()
+    .rangeRoundBands([0, width], .1);
+
+var y = d3.scale.linear()
+    .range([height, 0]);
+
+var xAxis = d3.svg.axis()
+    .scale(x)
+    .orient("bottom");
+
+var yAxis = d3.svg.axis()
+    .scale(y)
+    .orient("left")
+    .tickFormat(formatPercent);
+
+var tip = d3.tip()
+  .attr('class', 'd3-tip')
+  .attr('class', 'd3-tip')
+  .offset([-10, 0])
+  .html(function(d) {
+    return "<strong>Frequency:</strong> <span style='color:red'>" + d.frequency + "</span>";
+  })
+
+var svg_5 = d3.select("#chart_5").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+svg_5.call(tip);
+
+d3.tsv("data/data.tsv", type, function(error, data) {
+  x.domain(data.map(function(d) { return d.letter; }));
+  y.domain([0, d3.max(data, function(d) { return d.frequency; })]);
+
+  svg_5.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(xAxis);
+
+  svg_5.append("g")
+      .attr("class", "y axis")
+      .call(yAxis)
+    .append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 6)
+      .attr("dy", ".71em")
+      .style("text-anchor", "end")
+      .text("Frequency");
+
+  svg_5.selectAll(".bar")
+      .data(data)
+    .enter().append("rect")
+      .attr("class", "bar")
+      .attr("x", function(d) { return x(d.letter); })
+      .attr("width", x.rangeBand())
+      .attr("y", function(d) { return y(d.frequency); })
+      .attr("height", function(d) { return height - y(d.frequency); })
+      .on('mouseover', tip.show)
+      .on('mouseout', tip.hide)
+
+});
+
+function type(d) {
+  d.frequency = +d.frequency;
+  return d;
+}
