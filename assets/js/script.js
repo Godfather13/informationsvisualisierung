3dc77095c4059c6d60966f701d6a92bd8dc5dc7a
brehmju <julian.brehm@gmx.net>
2015-12-08 15:02:28 +0100

    slider also works with the pie and bar charts

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,10 +3,27 @@ $.getScript("assets/js/bootstrap.min.js", function() {
 $.getScript("assets/js/bootstrap-slider.js", function() {
 });
 
-$( document ).ready(function() {
-	$("#year").html("1990");
-});
+$(document).ready(function() {
 
+	$("#ex14").slider({
+		ticks: [0, 125, 150, 175, 180, 185, 190, 195, 200, 205, 210, 215],
+		ticks_labels: ['1900', '1925', '1950', '1975', '1980', '1985', '1990', '1995', '2000', '2005', '2010', '2015'],
+		ticks_positions: [0, 5, 10, 15, 22, 30, 37, 45, 55, 70, 85, 100],
+		ticks_snap_bounds: 0
+	});
+
+	$("#ex14").slider().change(function() {
+		var mySlider = $("#ex14").slider();
+		var value = mySlider.slider('getValue');
+		var yr = value + 1800;
+		$("#year").text(yr); 
+		
+		update_charts(yr);
+	});
+
+	$("#year").html("1990");
+
+});
 
 /* CHART 1 */
 
@@ -53,7 +70,7 @@ $( document ).ready(function() {
 // render function
 	function render(male, female){
 			
-		d3.select("svg").remove();
+		d3.select("#chart_1 svg").remove();
 				
 		// An SVG element with a bottom-right origin.
 			var svg_pop_chart = d3.select("#chart_1").append("svg")
@@ -172,6 +189,15 @@ $( document ).ready(function() {
 					}
 			});
 	}
+
+/* new year was selected */
+	function update_charts( selected_year ){
+		
+		render_char_1( selected_year );
+		render_char_2( selected_year );
+		render_char_3( selected_year );
+	}
+	
 	
 /* CHART 2 */
 
@@ -190,40 +216,47 @@ var pie = d3.layout.pie()
     .sort(null)
     .value(function(d) { return d.population; });
 
-var svg_2 = d3.select("#chart_2").append("svg")
-    .attr("width", width)
-    .attr("height", height)
-  .append("g")
-    .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+render_char_1( year );
+	
+function render_char_1( selected_year ){
+	
+	d3.select("#chart_2 svg").remove();
+	
+	var svg_2 = d3.select("#chart_2").append("svg")
+		.attr("width", width)
+		.attr("height", height)
+		.append("g")
+		.attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+	
+	d3.csv("data/pie_data.csv", function(error, udata) {
 
-d3.csv("data/pie_data.csv", function(error, udata) {
+		var data = udata.filter(function(row) {
+		return row["year"] == selected_year});
 
-	var data = udata.filter(function(row) {
-	return row["year"] == 1990});
+		data.forEach(function(d) {
+			d.population = +d.population;
+		});
 
-	data.forEach(function(d) {
-		d.population = +d.population;
+		var g = svg_2.selectAll(".arc")
+			.data(pie(data))
+			.enter().append("g")
+			.attr("class", "arc");
+
+		g.append("path")
+			.attr("d", arc)
+			.style("fill", function(d) { return color(d.data.age); });
+
+		g.append("text")
+			.attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")"; })
+			.attr("dy", ".35em")
+			.style("text-anchor", "middle")
+			.text(function(d) { return d.data.age; });
 	});
-
-	var g = svg_2.selectAll(".arc")
-		.data(pie(data))
-		.enter().append("g")
-		.attr("class", "arc");
-
-	g.append("path")
-		.attr("d", arc)
-		.style("fill", function(d) { return color(d.data.age); });
-
-	g.append("text")
-		.attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")"; })
-		.attr("dy", ".35em")
-		.style("text-anchor", "middle")
-		.text(function(d) { return d.data.age; });
-});
+}
 
 /* CHART 3 */
 
-var width = document.getElementById('chart_3').offsetWidth - 50,
+var width = document.getElementById('chart_3').offsetWidth - 100,
     height = 300,
     radius = Math.min(width, height) / 2;
 
@@ -238,35 +271,43 @@ var pie = d3.layout.pie()
     .sort(null)
     .value(function(d) { return d.population; });
 
-var svg_3 = d3.select("#chart_3").append("svg")
-    .attr("width", width)
-    .attr("height", height)
-  .append("g")
-    .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+render_char_2( year );
+	
+function render_char_2( selected_year ){
+	
+	d3.select("#chart_3 svg").remove();
+	
+	var svg_2 = d3.select("#chart_3").append("svg")
+		.attr("width", width)
+		.attr("height", height)
+		.append("g")
+		.attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+	
+	d3.csv("data/pie_data.csv", function(error, udata) {
 
-d3.csv("data/pie_data.csv", function(error, data) {
+		var data = udata.filter(function(row) {
+		return row["year"] == selected_year});
 
-  data.forEach(function(d) {
-    d.population = +d.population;
-  });
+		data.forEach(function(d) {
+			d.population = +d.population;
+		});
 
-  var g = svg_3.selectAll(".arc")
-      .data(pie(data))
-    .enter().append("g")
-      .attr("class", "arc");
+		var g = svg_2.selectAll(".arc")
+			.data(pie(data))
+			.enter().append("g")
+			.attr("class", "arc");
 
-  g.append("path")
-      .attr("d", arc)
-      .style("fill", function(d) { return color(d.data.age); });
+		g.append("path")
+			.attr("d", arc)
+			.style("fill", function(d) { return color(d.data.age); });
 
-  g.append("text")
-      .attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")"; })
-      .attr("dy", ".35em")
-      .style("text-anchor", "middle")
-      .text(function(d) { return d.data.age; });
-
-});
-
+		g.append("text")
+			.attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")"; })
+			.attr("dy", ".35em")
+			.style("text-anchor", "middle")
+			.text(function(d) { return d.data.age; });
+	});
+}
 
 /* CHART 4 */
 
@@ -299,47 +340,57 @@ var tip = d3.tip()
     return "<strong>Frequency:</strong> <span style='color:red'>" + d.frequency + "</span>";
   })
 
-var svg_4 = d3.select("#chart_4").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+  render_char_3( year );
+  
+ function render_char_3( selected_year ){
+  
+	d3.select("#chart_4 svg").remove();
+  
+	var svg_4 = d3.select("#chart_4").append("svg")
+		.attr("width", width + margin.left + margin.right)
+		.attr("height", height + margin.top + margin.bottom)
+	  .append("g")
+		.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-svg_4.call(tip);
+	svg_4.call(tip);
 
-d3.tsv("data/bar_data.tsv", type, function(error, data) {
-  x.domain(data.map(function(d) { return d.letter; }));
-  y.domain([0, d3.max(data, function(d) { return d.frequency; })]);
+	d3.csv("data/bar_data.csv", type, function(error, udata) {
+		
+		var data = udata.filter(function(row) {
+		return row["year"] == selected_year});
+		
+	  x.domain(data.map(function(d) { return d.letter; }));
+	  y.domain([0, d3.max(data, function(d) { return d.frequency; })]);
 
-  svg_4.append("g")
-      .attr("class", "x axis")
-      .attr("transform", "translate(0," + height + ")")
-      .call(xAxis);
+	  svg_4.append("g")
+		  .attr("class", "x axis")
+		  .attr("transform", "translate(0," + height + ")")
+		  .call(xAxis);
 
-  svg_4.append("g")
-      .attr("class", "y axis")
-      .call(yAxis)
-    .append("text")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 6)
-      .attr("dy", ".71em")
-      .style("text-anchor", "end")
-      .text("Frequency");
+	  svg_4.append("g")
+		  .attr("class", "y axis")
+		  .call(yAxis)
+		.append("text")
+		  .attr("transform", "rotate(-90)")
+		  .attr("y", 6)
+		  .attr("dy", ".71em")
+		  .style("text-anchor", "end") ;
 
-  svg_4.selectAll(".bar")
-      .data(data)
-    .enter().append("rect")
-      .attr("class", "bar")
-      .attr("x", function(d) { return x(d.letter); })
-      .attr("width", x.rangeBand())
-      .attr("y", function(d) { return y(d.frequency); })
-      .attr("height", function(d) { return height - y(d.frequency); })
-      .on('mouseover', tip.show)
-      .on('mouseout', tip.hide)
+	  svg_4.selectAll(".bar")
+		  .data(data)
+		.enter().append("rect")
+		  .attr("class", "bar")
+		  .attr("x", function(d) { return x(d.letter); })
+		  .attr("width", x.rangeBand())
+		  .attr("y", function(d) { return y(d.frequency); })
+		  .attr("height", function(d) { return height - y(d.frequency); })
+		  .on('mouseover', tip.show)
+		  .on('mouseout', tip.hide)
 
-});
+	});
 
-function type(d) {
-  d.frequency = +d.frequency;
-  return d;
+	function type(d) {
+	  d.frequency = +d.frequency;
+	  return d;
+	}
 }
